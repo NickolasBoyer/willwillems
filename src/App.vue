1781dbf496e5d15c82221ad5567071a1caf7c4f6
NickolasBoyer <rut.willems@gmail.com>
2018-10-05 00:01:53 +0200

    welcome page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    app-welcome-page
+    a.contact-link( href="mailto:will@nickolasboyer.com" )
+      img( src="./assets/contact-img.svg" )
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppWelcomePage from './components/AppWelcomePage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AppWelcomePage
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+html {
+  font-family: 'SF Pro', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  background-color: black;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+
+.contact-link {
+  position: fixed;
+  right: 20px;
+  bottom: 20px;
+  color: white;
+
+  // &::before {
+  //   content: '';
+  //   display: inline-block;
+  //   height: 10px;
+  //   width: 10px;
+  //   border-radius: 50%;
+  //   margin: 3px;
+  //   background-color: red;
+  // }
 }
 </style>
---
+++ src/components/AppWelcomePage.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+  .section.welcome-section
+    .image-wrapper
+      .fade-overlay
+      .gradient-background
+      img.headshot-image( src="./AppWelcomePage/assets/headshot.png" )
+    .text-section
+      strong.sub-header Hi there, I'm
+      h1.title Will Willems
+      p.body Designer developer and web development consultant @NickolasBoyer. I’m mainly working with Vue.js these days with a strong focus on PWA's.
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.welcome-section {
+  display: flex;
+  flex-direction: column;
+}
+.image-wrapper {
+  position: relative;
+  width: 100vw;
+  
+  .fade-overlay {
+    z-index: 1;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    background-image: linear-gradient(-180deg, rgba(0,0,0,0.00) 75%, #000000 100%);
+  }
+
+  .gradient-background {
+    z-index: -1;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    opacity: 0.8;
+    background-image: linear-gradient(-134deg, #3023AE 0%, #C96DD8 100%);  }
+
+  img {
+    width: 100vw;
+    filter: grayscale(100%);
+    transition: filter 1s ease-in-out;
+  }
+
+  &:hover {
+    img {
+      filter: none;
+    }
+  }
+}
+
+.text-section {
+  margin: 0 20px;
+
+  .sub-header {
+    color: #EA7928;
+  }
+
+  .title {
+    margin: 10px 0 5px 0;
+    font-size: 3rem;
+  }
+
+  .body {
+    margin: 0;
+    line-height: 1.8em;
+  }
+}
+</style>
