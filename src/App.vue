41af719525727c4021f324273ac1343d79072f9f
NickolasBoyer <rut.willems@gmail.com>
2018-11-09 11:26:12 +0100

    add subscribe overlay start & seperate styles

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,25 @@
 <template lang="pug">
   #app
-    the-welcome-page
+    TheWelcomePage
     a.contact-link( href="mailto:will@nickolasboyer.com" )
       img( src="./assets/contact-img.svg" )
+    TheSubscribeOverlay
 </template>
 
 <script>
 import TheWelcomePage from './components/TheWelcomePage.vue'
+import TheSubscribeOverlay from './components/TheSubscribeOverlay.vue'
 
 export default {
   name: 'app',
   components: {
-    TheWelcomePage
+    TheWelcomePage,
+    TheSubscribeOverlay
   }
 }
 </script>
 
 <style lang="scss">
-html {
-  font-family: 'SF Pro', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: white;
-  background-color: black;
-}
-
-body {
-  padding: 0;
-  margin: 0;
-}
-
 .contact-link {
   position: fixed;
   right: 20px;
---
+++ src/components/TheSubscribeOverlay.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .overlay.subscribe-overlay
+    h1 Subscribe
+    p And rather than just blasting out a bunch of links, you want to analyze each article.What did you get out of it? How do you think it will help the reader and their business?
+    input( type="text" )
+    br
+    button YES
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.overlay {
+  z-index: 2;
+  @include overlay-fill();
+  background-color: $black;
+  text-align: center;
+  padding: 20px;
+}
+</style>
