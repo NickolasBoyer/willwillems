ca3703d6f145a6ea1c085f804fdf019a522a29c9
Rutger Willems <rut.willems@gmail.com>
2019-01-31 16:18:34 +0700

    Add GA to project

--- .vuepress/theme/enhanceApp.js
+++ .vuepress/theme/enhanceApp.js
@@ -1,9 +1,10 @@
 // import VueRouter from 'vue-router'
 
 // import Home from './components/Home.vue'
+import VueAnalytics from 'vue-analytics'
 
 export default ({
-  // Vue, // the version of Vue being used in the VuePress app
+  Vue, // the version of Vue being used in the VuePress app
   // options, // the options for the root Vue instance
   router // the router instance for the app
   // siteData // site metadata
@@ -12,4 +13,9 @@ export default ({
   require('./styles/index.scss')
   // add routes
   // router.addRoutes([{ path: '/foo', component: Home }])
+  // Setup analytics
+  Vue.use(VueAnalytics, {
+    id: 'UA-116621358-3',
+    router
+  })
 }
